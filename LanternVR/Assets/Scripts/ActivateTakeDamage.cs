49512bfd87ada40acc2044b87e7c551eecc309c1
Stephen Ogden <42942300+sjogden@users.noreply.github.com>
2019-02-20 20:35:27 -0500

    Health and Damage scripts
    ActivateTakeDamage triggers TakeDamage.SubtractHealth on collision.  SubtractHealth decrements health of object and deletes all associated objects if it drops to 0.

---
+++ LanternVR/Assets/Scripts/ActivateTakeDamage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateTakeDamage : MonoBehaviour {
+
+    private bool triggered = false; // when false, allows object to call TakeDamage
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (triggered)
+        {
+            GetComponent<TakeDamage>().SubtractHealth(); // call TakeDamage on an object collision
+        }
+        else
+        {
+            triggered = true; // only allow call to TakeDamage once
+        }
+    }
+}
---
+++ LanternVR/Assets/Scripts/TakeDamage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeDamage : MonoBehaviour {
+
+    public int health; // records the health of the object.
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SubtractHealth ()
+    {
+        health--; // decrements the health of the object by one
+        if (health <= 0)
+        {
+            Destroy(transform.root.gameObject); // destroy the object and all associated objects if it loses all health
+        }
+    }
+}
