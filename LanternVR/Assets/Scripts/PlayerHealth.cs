4e57d17e53e1fe56f2ea8bf0fbe079c8db244b18
Dominatorg318 <dominatorg318@gmail.com>
2019-02-27 15:08:10 -0500

    Added PlayerHealth Script

---
+++ LanternVR/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth
+{
+
+    public int health;
+    public int healthMax;
+
+    public PlayerHealth(int healthMax)
+    {
+        this.healthMax = healthMax;
+        health = healthMax;
+    }
+
+    //gets the current level of health
+    public int getHealth()
+    {
+        return health;
+    }
+
+    // deals damage to the subject 
+    public void damage(int damageAmount)
+    {
+        health -= damageAmount;
+        if (health < 0)
+        {
+            health = 0;
+        }
+    }
+
+    // heals the subject
+    public void heal(int healAmount)
+    {
+        health += healAmount;
+        if (health > healthMax)
+        {
+            health = healthMax;
+        }
+    }
+
+
+}
